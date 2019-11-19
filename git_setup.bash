12252aeb94f74f920abf0b2fb30bb9bf0e448297
rnetodev <rneto@rneto.dev>
2019-11-19 15:38:35 -0300

    Git setup

---
+++ git_setup.bash
@@ -0,0 +1,12 @@
+git config --global user.name rnetodev
+git config --global user.email rneto@rneto.dev
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=9003600'
+git config --global core.autocrlf false
+git config --global core.safecrlf false
+git config --global color.ui true
+git config --global format.pretty oneline
+git config --global core.autocrlf false
+git config --global push.default simple
+git config --global http.postbuffer 1048576000
+git config --global core.compression 0
