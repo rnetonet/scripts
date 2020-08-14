de322b2bda39e4d9d951a64148c2f56956e4cc8c
rnetonet <rneto@rneto.net>
2020-08-14 00:16:17 -0300

    vscode

--- setup_aptget_build_and_python_install.bash
+++ setup_aptget_build_and_python_install.bash
@@ -91,6 +91,17 @@ sudo apt-get install -y gnupg
 # keep sudo
 sudo echo
 
+# vscode
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y code
+
+# keep sudo
+sudo echo
+
 # docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
