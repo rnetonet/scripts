5ae2329a9a53a78e07b6732e1470df546011528a
rnetonet <rneto@rneto.net>
2020-12-19 00:19:19 -0300

    add git setup if

---
+++ git_setup_if.sh
@@ -0,0 +1,12 @@
+git config user.name ruivaldo.lobao
+git config user.email ruivaldo.lobao@ifbaiano.edu.br
+git config credential.helper cache
+git config credential.helper 'cache --timeout=9003600'
+git config core.autocrlf false
+git config core.safecrlf false
+git config color.ui true
+git config format.pretty oneline
+git config core.autocrlf false
+git config push.default simple
+git config http.postbuffer 1048576000
+git config core.compression 0
